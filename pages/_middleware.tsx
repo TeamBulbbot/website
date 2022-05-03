b5d8a551f4bf0bc2e5fb53f4e2ffdeb3c88c8262
y3ll0w <46920058+y3ll0wlife@users.noreply.github.com>
2022-05-03 23:28:19 +0200

    redploy

--- pages/_middleware.tsx
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-const Middleware = (req: NextRequest) => {
-	if (req.nextUrl.pathname === req.nextUrl.pathname.toUpperCase()) return NextResponse.next();
-	return NextResponse.redirect(`${req.nextUrl.origin}${req.nextUrl.pathname.toUpperCase()}`);
-};
-
-export default Middleware;
