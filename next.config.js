796bc7010ddb8066eed14422456e4746bd6137e5
y3ll0w <46920058+y3ll0wlife@users.noreply.github.com>
2022-05-03 21:54:29 +0200

    changed image loader

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ["@svgr/webpack"],
-		});
-		return config;
+	images: {
+		loader: "akamai",
+		path: "",
 	},
+
 	async redirects() {
 		return [
 			{
